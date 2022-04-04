aa62ce21f29a3d73a39a234e802036a3dd1ebe61
Aanttrax <Arielrt20@gmail.com>
2022-04-03 21:46:49 -0400

    update

--- client/photoshare/src/components/HomePage.js
+++ client/photoshare/src/components/HomePage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import user from '../img/user.png';
 
 import './HomePage.css'
 
@@ -9,18 +10,33 @@ function HomePage() {
 
     return (
         <div>
-                <select>
+            <div className="principal">
+                <div className="user">
+                    <img src = {user} alt='user' width="50" height="50"/>
+                    <p>Nombre de Usuario</p>
+                </div>
+                <div>
+                    <input className="search" type= 'text' name='buscar'>
+                    </input>
+                </div>
+            </div>
+            <div className="menu">
+                <select className="select">
                     <option value=''>Opciones</option>
                     <option value='Edit'>Editar perfil</option>
                 
                 </select>
                 <div>
                     {Array.isArray(btn) && btn.map((c,i)=>(
-                        <button key ={i}>
+                        <button 
+                            key ={i}
+                            className = 'btn'>
                             {c}
                         </button>
                     ))}
                 </div>
+            </div>
+                
         </div>
     )
 };
