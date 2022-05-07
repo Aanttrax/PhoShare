dad7c8384041a35c3192104f65d3a965f8a99c42
Aanttrax <Arielrt20@gmail.com>
2022-05-07 00:56:34 -0400

    fix2

--- PhotoShare/client/src/store/store.js
+++ PhotoShare/client/src/store/store.js
@@ -1,10 +1,20 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from '../reducer/reducer';
+// import { createStore, applyMiddleware, compose } from "redux";
+// import thunk from "redux-thunk";
+// import rootReducer from '../reducer/reducer';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// export const store = createStore(
+//     rootReducer,
+//     composeEnhancers(applyMiddleware(thunk))
+//   )
+
+  import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reducers from '../reducer/reducer';
 
 export const store = createStore(
-    rootReducer,
-    composeEnhancers(applyMiddleware(thunk))
-  )
+  reducers,
+  composeWithDevTools(applyMiddleware(thunk))
+);
